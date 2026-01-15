f4ced02b352ab52e92cc4f041072649d823c606d
Ayush Maiti <ayush.maiti39@gmail.com>
2026-01-15 15:41:10 +0000

    CD10 updated

--- day6-aws-automation/drills.py
+++ day6-aws-automation/drills.py
@@ -7,10 +7,10 @@ s3 = boto3.client('s3')
 print("Connection Successful!")
 
 bucket_name = f"devops-bootcamp-{uuid.uuid4().hex[:6]}"
-print(f"Creating bucket: {bucket_name}")
-s3.create_bucket(Bucket=bucket_name)
+# print(f"Creating bucket: {bucket_name}")
+# s3.create_bucket(Bucket=bucket_name)
 
 print(f"Bucket {bucket_name} Created.")
 
 response = s3.list_buckets()
-print(response['Buckets']['Name'])
+print(response['Buckets'])
